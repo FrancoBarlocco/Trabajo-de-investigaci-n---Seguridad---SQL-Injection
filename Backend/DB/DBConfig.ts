0f63688247db205d2008732634305b00cfe8c26e
NicoLorenzo1 <lorenzonicolas.48@gmail.com>
2024-06-23 20:27:37 -0300

    finished

--- Backend/DB/DBConfig.ts
+++ Backend/DB/DBConfig.ts
@@ -8,7 +8,8 @@ import dotenv from 'dotenv';
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE
+      database: process.env.DB_DATABASE,
+      multipleStatements: true
     };
   }
 
--- Backend/UserController.ts
+++ Backend/UserController.ts
@@ -5,23 +5,22 @@ import bcryptjs from 'bcryptjs';
 export const login = async (req: Request, res: Response) => {
     const username = req.body.Name;
     const password = req.body.Password;
-    console.log("llegooooo" + username + password);
 
     if (!username || !password) {
         return res.status(400).json({ msg: 'Username and password are required' });
     }
 
     try {
-        // Buscar usuario en la tabla Users
-        const [userResult]: any[] = await connection.execute(
-            'SELECT * FROM Users WHERE Name = ?',
-            [username]
-        );
+            // Buscar usuario en la tabla Users
+
+        const query = `SELECT * FROM Users WHERE Name = ? AND Password = ?`
+        const [userResult]: any[] = await connection.execute(query, [username, password]);
+
 
         // Verificar si se encontró un usuario
         if (userResult.length === 0) {
             return res.status(401).json({
-                msg: 'Name is not registered'
+                msg: 'Name or password is incorrect'
             });
         }
 
@@ -29,20 +28,12 @@ export const login = async (req: Request, res: Response) => {
         const user = userResult[0];
         let userType = '';
 
-        // Verificar la contraseña
-        const validPassword = bcryptjs.compareSync(password, user.Password);
-        if (!validPassword) {
-            return res.status(401).json({
-                msg: 'Password is not correct'
-            });
-        } else {
-            return res.status(200).json({
-                msg: 'Logged in successfully!',
-                userName: user.Name,
-                userId: user.Ci,
-                userType: userType, 
-            });
-        }
+        return res.status(200).json({
+            msg: 'Logged in successfully!',
+            userName: user.Name,
+            userId: user.Ci,
+            userType: userType,
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({ msg: 'INTERNAL_SERVER_ERROR' });
@@ -52,21 +43,20 @@ export const login = async (req: Request, res: Response) => {
 export const loginInjection = async (req: Request, res: Response) => {
     const username = req.body.Name;
     const password = req.body.Password;
-    console.log("llegooooo" + username + password);
 
     if (!username || !password) {
         return res.status(400).json({ msg: 'Username and password are required' });
     }
-    console.log("consulta `SELECT * FROM Users WHERE Name =" + username)
     try {
         // Buscar usuario en la tabla Users
-        const query = `SELECT * FROM Users WHERE Name = '${username}'`;
+        
+        const query = `SELECT * FROM Users WHERE Name = '${username}' AND Password = '${password}'`;
         const [userResult]: any[] = await connection.execute(query);
 
         // Verificar si se encontró un usuario
         if (userResult.length === 0) {
             return res.status(401).json({
-                msg: 'Name is not registered'
+                msg: 'Name or password is incorrect'
             });
         }
 
@@ -74,20 +64,12 @@ export const loginInjection = async (req: Request, res: Response) => {
         const user = userResult[0];
         let userType = '';
 
-        // Verificar la contraseña
-        const validPassword = bcryptjs.compareSync(password, user.Password);
-        if (!validPassword) {
-            return res.status(401).json({
-                msg: 'Password is not correct'
-            });
-        } else {
-            return res.status(200).json({
-                msg: 'Logged in successfully!',
-                userName: user.Name,
-                userId: user.Ci,
-                userType: userType, 
-            });
-        }
+        return res.status(200).json({
+            msg: 'Logged in successfully!',
+            userName: user.Name,
+            userId: user.Ci,
+            userType: userType,
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({ msg: 'INTERNAL_SERVER_ERROR' });
